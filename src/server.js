4d440f3afc03f2ca4c43ba6380f08c3db82d4b00
Lucas Pereira <lucaspc500@gmail.com>
2026-06-25 13:54:04 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const reportsRouter = require('./routes/reports');
 const express = require('express');
 const cors = require('cors');
 const pool = require('./db/postgres');
